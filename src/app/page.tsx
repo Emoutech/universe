d040026d44951996df4dd785fb341ae910a88ad7
Everton Mourinho <everton.mourinho@hotmail.com>
2024-12-08 16:53:15 -0300

    chore: altera titulo para workspaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // sections
-import { HomeView } from 'src/sections/home/view';
+import { HomeView } from "src/sections/home/view";
 
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimal: The starting point for your next project',
+  title: "Flechei: Workspace",
 };
 
 export default function HomePage() {
